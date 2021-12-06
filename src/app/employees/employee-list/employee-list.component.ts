8806983ad369abe8096b8f43471a00330c343f85
Mulyono Putra <mulyono.putra@salt.co.id>
2021-12-06 11:37:48 +0700

    Component Input properties change detection using ngOnChanges.

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -10,9 +10,22 @@ import { EmployeeService } from 'src/app/services/employee.service';
 export class EmployeeListComponent implements OnInit {
 
   employees!: Employee[];
+  employeeDisplay!: Employee;
+  private arrayOfIndex: number = 1;
   constructor(private employee: EmployeeService) {}
 
   ngOnInit(): void {
     this.employees = this.employee.findAll();
+    this.employeeDisplay = this.employees[0];
+  }
+
+  nextEmployee(){
+    if (this.arrayOfIndex <= 2) {
+      this.employeeDisplay = this.employees[this.arrayOfIndex];
+      this.arrayOfIndex++;
+    } else {
+      this.employeeDisplay = this.employees[0];
+      this.arrayOfIndex = 1;
+    }
   }
 }
--- src/app/employees/employee-view/employee-view.component.ts
+++ src/app/employees/employee-view/employee-view.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { Employee } from 'src/app/models/employee';
 
 @Component({
   selector: 'app-employee-view',
   templateUrl: './employee-view.component.html',
-  styleUrls: ['./employee-view.component.scss']
+  styleUrls: ['./employee-view.component.scss'],
 })
-export class EmployeeViewComponent implements OnInit {
-  @Input() employee!: Employee
-  
-  constructor() { }
+export class EmployeeViewComponent implements OnInit, OnChanges {
+  @Input() employee!: Employee;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) { 
+    const previousEmployee = <Employee>changes.employee.previousValue;
+    const currentEmployee = <Employee>changes.employee.currentValue;
+
+    console.log('Previous : ' + (previousEmployee ? previousEmployee.fullname : 'NULL'));
+    console.log('Current : ' + currentEmployee.fullname);
   }
 
+  ngOnInit(): void {}
 }
