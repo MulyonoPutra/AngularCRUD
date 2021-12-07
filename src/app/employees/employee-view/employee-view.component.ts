25854b14445ae9cdc8d478f4ee3b521018c54ca9
Mulyono Putra <mulyono.putra@salt.co.id>
2021-12-07 20:19:16 +0700

    optional route parameters

--- src/app/employees/employee-view/employee-view.component.ts
+++ src/app/employees/employee-view/employee-view.component.ts
@@ -5,6 +5,7 @@ import {
   OnInit,
   SimpleChanges,
 } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Employee } from 'src/app/models/employee';
 
 @Component({
@@ -13,7 +14,7 @@ import { Employee } from 'src/app/models/employee';
   styleUrls: ['./employee-view.component.scss'],
 })
 export class EmployeeViewComponent implements OnInit {
-
+  selectedId!: number;
   // ngOnChanges
   // We get all the changes instead of just the changes related to a single property
   // useful when multiple properties changes
@@ -21,17 +22,20 @@ export class EmployeeViewComponent implements OnInit {
   // Setter
   // Property setter is specific to a given property, so we only get changes of that specific property.
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.selectedId = +this.route.snapshot.paramMap.get('id')!;
+  }
 
   private _employee!: Employee;
   private _employeeId!: number;
 
-
   @Input()
   set employeeId(value: number) {
-    console.log('changed from ' + JSON.stringify(this._employeeId) + ' to ' + value);
+    console.log(
+      'changed from ' + JSON.stringify(this._employeeId) + ' to ' + value
+    );
     this._employeeId = value;
   }
 
@@ -41,16 +45,20 @@ export class EmployeeViewComponent implements OnInit {
 
   @Input()
   set employee(value: Employee) {
-    console.log('changed from  ' + JSON.stringify(this._employee) + ' to ' + JSON.stringify(value));
-    this._employee = value
+    console.log(
+      'changed from  ' +
+        JSON.stringify(this._employee) +
+        ' to ' +
+        JSON.stringify(value)
+    );
+    this._employee = value;
   }
 
   get employee(): Employee {
-    return this._employee
+    return this._employee;
   }
 
   getEmployeeNameAndGender(): string {
-    return this.employee.fullname + ' ' + this.employee.gender
+    return this.employee.fullname + ' ' + this.employee.gender;
   }
-
 }
