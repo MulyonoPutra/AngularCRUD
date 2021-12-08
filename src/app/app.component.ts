3d40d2c42441364e1a567265cdc0fae0c81f7833
Mulyono Putra <mulyono.putra@salt.co.id>
2021-12-08 15:20:26 +0700

    Angular route loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  Event,
+  Router,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'employee-management';
+  showLoadingIndicator!: boolean;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((routeEvent: Event) => {
+      if (routeEvent instanceof NavigationStart) {
+        this.showLoadingIndicator = true;
+      }
+
+      if (routeEvent instanceof NavigationEnd) {
+        this.showLoadingIndicator = false;
+      }
+
+      if (
+        routeEvent instanceof NavigationEnd ||
+        routeEvent instanceof NavigationError ||
+        routeEvent instanceof NavigationCancel
+      ) {
+        this.showLoadingIndicator = false;
+      }
+    });
+  }
 }
